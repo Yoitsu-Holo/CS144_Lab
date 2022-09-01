a25b8d3e0f86f78c4980812821c39cfc1422c2a1
YoitsuHolo <YoitsuHolo@outlook.com>
2022-09-01 05:58:04 +0000

    lab 4 (rebuild point)

--- libsponge/byte_stream.hh
+++ libsponge/byte_stream.hh
@@ -1,9 +1,8 @@
 #ifndef SPONGE_LIBSPONGE_BYTE_STREAM_HH
 #define SPONGE_LIBSPONGE_BYTE_STREAM_HH
 
+#include <deque>
 #include <string>
-#include <assert.h>
-
 //! \brief An in-order byte stream.
 
 //! Bytes are written on the "input" side and read from the "output"
@@ -12,49 +11,79 @@
 class ByteStream {
   private:
     // Your code here -- add private members as necessary.
-    std::string buff;    // buffer
-    size_t front, back;  // two pointer, pointed buffer front and back
-    bool isInputEnd;
-    size_t totin, totout;
-    //& Hint: This doesn't need to be a sophisticated data structure at
-    //& all, but if any of your tests are taking longer than a second,
-    //& that's a sign that you probably want to keep exploring
-    //& different approaches.
 
+    // Hint: This doesn't need to be a sophisticated data structure at
+    // all, but if any of your tests are taking longer than a second,
+    // that's a sign that you probably want to keep exploring
+    // different approaches.
+    std::deque<char> buffer;
+    size_t capacity;
+    bool end_write;
+    bool end_read;
+    size_t written_bytes;
+    size_t read_bytes;
     bool _error{};  //!< Flag indicating that the stream suffered an error.
 
   public:
-    //^ Construct a stream with room for `capacity` bytes.
+    //! Construct a stream with room for `capacity` bytes.
     ByteStream(const size_t capacity);
 
-    //^ \name "Input" interface for the writer
-    //! @{
-    size_t write(const std::string &data);  //* Write a string of bytes into the stream. Write as many
-                                            //* as will fit, and return how many were written.
-                                            //* \returns the number of bytes accepted into the stream
-    size_t remaining_capacity() const;      //* \returns the number of additional bytes that the stream has space for
-    void end_input();                       //* Signal that the byte stream has reached its ending
-    void set_error() { _error = true; }     //* Indicate that the stream suffered an error.
-    //! @}
-
-    //^ \name "Output" interface for the reader
+    //! \name "Input" interface for the writer
     //!@{
-    std::string peek_output(const size_t len) const;  //* Peek at next "len" bytes of the stream
-                                                      //* \returns a string
-    void pop_output(const size_t len);                //* Remove bytes from the buffer
-    std::string read(const size_t len);    //* Read (i.e., copy and then pop) the next "len" bytes of the stream
-                                           //*\returns a string
-    bool input_ended() const;              //* \returns `true` if the stream input has ended
-    bool error() const { return _error; }  //* \returns `true` if the stream has suffered an error
-    size_t buffer_size() const;            //* \returns the maximum amount that can currently be read from the stream
-    bool buffer_empty() const;             //* \returns `true` if the buffer is empty
-    bool eof() const;                      //* \returns `true` if the output has reached the ending
-    //! @}
 
-    //^ \name General accounting
+    //! Write a string of bytes into the stream. Write as many
+    //! as will fit, and return how many were written.
+    //! \returns the number of bytes accepted into the stream
+    size_t write(const std::string &data);
+
+    //! \returns the number of additional bytes that the stream has space for
+    size_t remaining_capacity() const;
+
+    //! Signal that the byte stream has reached its ending
+    void end_input();
+
+    //! Indicate that the stream suffered an error.
+    void set_error() { _error = true; }
+    //!@}
+
+    //! \name "Output" interface for the reader
     //!@{
-    size_t bytes_written() const;  //* Total number of bytes written
-    size_t bytes_read() const;     //* Total number of bytes popped
+
+    //! Peek at next "len" bytes of the stream
+    //! \returns a string
+    std::string peek_output(const size_t len) const;
+
+    //! Remove bytes from the buffer
+    void pop_output(const size_t len);
+
+    //! Read (i.e., copy and then pop) the next "len" bytes of the stream
+    //! \returns a string
+    std::string read(const size_t len);
+
+    //! \returns `true` if the stream input has ended
+    bool input_ended() const;
+
+    //! \returns `true` if the stream has suffered an error
+    bool error() const { return _error; }
+
+    //! \returns the maximum amount that can currently be read from the stream
+    size_t buffer_size() const;
+
+    //! \returns `true` if the buffer is empty
+    bool buffer_empty() const;
+
+    //! \returns `true` if the output has reached the ending
+    bool eof() const;
+    //!@}
+
+    //! \name General accounting
+    //!@{
+
+    //! Total number of bytes written
+    size_t bytes_written() const;
+
+    //! Total number of bytes popped
+    size_t bytes_read() const;
     //!@}
 };
 
--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -8,72 +8,90 @@
 // You will need to add private members to the class declaration in `stream_reassembler.hh`
 
 template <typename... Targs>
-void DUMMY_CODE(Targs &&.../* unused */) {}
+void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
 StreamReassembler::StreamReassembler(const size_t capacity)
-    : _output(capacity), _capacity(capacity), unassembledBuffer(), unreadBuffer(), posEOF(ULONG_LONG_MAX) {}
+    : unass_base(0)
+    , unass_size(0)
+    , _eof(0)
+    , buffer(capacity, '\0')
+    , bitmap(capacity, false)
+    , _output(capacity)
+    , _capacity(capacity) {}
+
+//! \details This functions calls just after pushing a substring into the
+//! _output stream. It aims to check if there exists any contiguous substrings
+//! recorded earlier can be push into the stream.
+void StreamReassembler::check_contiguous() {
+    string tmp = "";
+    while (bitmap.front()) {
+        // cout<<"check one more contiguous substring"<<endl;
+        tmp += buffer.front();
+        buffer.pop_front();
+        bitmap.pop_front();
+        buffer.push_back('\0');
+        bitmap.push_back(false);
+    }
+    if (tmp.length() > 0) {
+        // cout << "push one contiguous substring with length " << tmp.length() << endl;
+        _output.write(tmp);
+        unass_base += tmp.length();
+        unass_size -= tmp.length();
+    }
+}
 
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    // if (index < 1000 && data.size() < 20) {
-    //     for (auto &&c : data)
-    //         debug << int(c) << ",";
-    //     debug << data << " | " << index << " | " << eof << endl;
-    // }
-    size_t firstUnread = _output.bytes_written();
-    size_t firstUnassembled = _output.bytes_written() + unreadBuffer.size();
-
-    // calc the position of eof
-    if (eof)
-        posEOF = index + data.size();
-
-    // if substring is totally pushed, ignore it
-    if (index + data.size() < firstUnread)
-        return;
-
-    if (index > posEOF)
-        return;
-
-    // save unassembled data
-    if (index + data.size() >= firstUnassembled)
-        for (size_t i = 0, pos = index; i < data.size() && pos < posEOF; i++, pos++) {
-            if (pos >= firstUnassembled)
-                unassembledBuffer[pos] = data[i];
-            //! testpoint has bug??
-            else if (pos >= firstUnread)
-                unreadBuffer[unreadBuffer.size() - (firstUnassembled - pos)] = data[i];
-        }
-
-    // put assembled data to unread buffer
-    while (unassembledBuffer.find(firstUnassembled) != unassembledBuffer.end()) {
-        unreadBuffer.push_back(unassembledBuffer[firstUnassembled]);
-        unassembledBuffer.erase(unassembledBuffer.find(firstUnassembled));
-        firstUnassembled++;
+    if (eof) {
+        _eof = true;
     }
-
-    // put assembled data to _output buffer
-    // if (unreadBuffer.size() < 20) {
-    //     for (auto &&c : unreadBuffer)
-    //         debug << int(c) << ",";
-    //     debug << "bufed data: " << unreadBuffer << endl;
-    // }
-    int byteWritten = _output.write(unreadBuffer);
-    unreadBuffer = unreadBuffer.substr(byteWritten);
-
-    if (_output.bytes_written() == posEOF)
+    size_t len = data.length();
+    if (len == 0 && _eof && unass_size == 0) {
         _output.end_input();
+        return;
+    }
+    // ignore invalid index
+    if (index >= unass_base + _capacity) return;
 
-    return;
+    if (index >= unass_base) {
+        int offset = index - unass_base;
+        size_t real_len = min(len, _capacity - _output.buffer_size() - offset);
+        if (real_len < len) {
+            _eof = false;
+        }
+        for (size_t i = 0; i < real_len; i++) {
+            if (bitmap[i + offset])
+                continue;
+            buffer[i + offset] = data[i];
+            bitmap[i + offset] = true;
+            unass_size++;
+        }
+    } else if (index + len > unass_base) {
+        int offset = unass_base - index;
+        size_t real_len = min(len - offset, _capacity - _output.buffer_size());
+        if (real_len < len - offset) {
+            _eof = false;
+        }
+        for (size_t i = 0; i < real_len; i++) {
+            if (bitmap[i])
+                continue;
+            buffer[i] = data[i + offset];
+            bitmap[i] = true;
+            unass_size++;
+        }
+    }
+    check_contiguous();
+    if (_eof && unass_size == 0) {
+        _output.end_input();
+    }
 }
 
-size_t StreamReassembler::unassembled_bytes() const { return {unassembledBuffer.size()}; }
+size_t StreamReassembler::unassembled_bytes() const { return unass_size; }
 
-bool StreamReassembler::empty() const {
-    if (unreadBuffer.size() + unassembledBuffer.size())
-        return false;
-    return true;
-}
+bool StreamReassembler::empty() const { return unass_size == 0; }
+
+size_t StreamReassembler::ack_index() const { return unass_base; }
